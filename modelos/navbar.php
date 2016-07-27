f14159cba7e1eca9a50d6c3e6208b97bb8e94e94
Arthur Hosang <arthur.rhc@gmail.com>
2016-07-26 22:20:19 -0300

    Corrigido o link principal do menu

--- modelos/navbar.php
+++ modelos/navbar.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="<?php echo $_SERVER['REQUEST_URI'] ?>">Gerenciador de Projetos</a>
+            <a class="navbar-brand" href="javascript:location.reload();">Gerenciador de Projetos</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
